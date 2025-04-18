c654191dc9ae4d7132323e5bc00ab23313f9f254
Spencer Attick <spencer.attick@gmail.com>
2025-04-18 10:45:57 -0400

    add gates to button clicks

--- fancy-counter/src/CountButton.jsx
+++ fancy-counter/src/CountButton.jsx
@@ -1,22 +1,28 @@
 import { MinusIcon, PlusIcon } from "@radix-ui/react-icons";
 
 export default function CountButton({ setCount, type }) {
-
   const handleClick = () => {
-    setCount(prev => {
-        if (type === 'minus') {
-            return prev - 1
+    setCount((prev) => {
+      if (type === "minus") {
+        const newCount = prev - 1;
+        if (newCount < 0) {
+          return 0;
         } else {
-            return prev + 1
+          return newCount;
         }
+      } else {
+        const newCount = prev + 1;
+        if (newCount > 5) {
+          return 5;
+        } else {
+          return newCount;
+        }
+      }
     });
-  }
+  };
 
   return (
-    <button
-      className="count-btn"
-      onClick={handleClick}
-    >
+    <button className="count-btn" onClick={handleClick}>
       {type === "minus" ? (
         <MinusIcon className="count-btn-icon" />
       ) : (
