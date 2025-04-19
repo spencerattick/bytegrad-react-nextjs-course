fe877032eca9b86e798e0a180734e79e8d03c75d
Spencer Attick <spencer.attick@gmail.com>
2025-04-19 10:29:03 -0400

    add stats logic

--- word-analytics/src/Stats.jsx
+++ word-analytics/src/Stats.jsx
@@ -1,5 +1,20 @@
-export default function Stats() {
+export default function Stats({text}) {
+    const textLength = text.length
   return (
-    <section className="stats">Stats</section>
-  )
+    <section className="stats">
+      <Stat number={text.split(' ').length} label='Words'/>
+      <Stat number={textLength} label='Characters'/>
+      <Stat number={280 - textLength} label='Instagram'/>
+      <Stat number={2200 - textLength} label='Facebook'/>
+    </section>
+  );
+}
+
+function Stat({label, number}) {
+  return (
+    <section className="stat">
+      <span className="stat__number">{number || 0}</span>
+      <h2 className="second-heading">{label}</h2>
+    </section>
+  );
 }
