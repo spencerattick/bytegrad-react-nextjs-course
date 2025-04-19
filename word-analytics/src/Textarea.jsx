2c7dd8f6e243b125b0d390c382a89907492b8a73
Spencer Attick <spencer.attick@gmail.com>
2025-04-19 07:25:19 -0400

    add additional warning logic

--- word-analytics/src/Textarea.jsx
+++ word-analytics/src/Textarea.jsx
@@ -1,5 +1,33 @@
+import { useState } from "react";
+import Warning from "./Warning";
+
 export default function Textarea() {
+  const [text, setText] = useState("");
+  const [warningText, setWarningText] = useState("");
+
+  const handleOnChange = (event) => {
+    let newText = event.target.value;
+    if (newText.includes("<script>")) {
+      newText = newText.replace("<script>", "");
+      setWarningText("No script tags allowed!");
+    } else if (newText.includes("@")) {
+      setWarningText("No @ symbols are allowed!");
+      newText = newText.replace("@", "");
+    } else {
+      setWarningText("");
+    }
+    setText(newText);
+    console.log("text:", text);
+  };
   return (
-    <textarea className="textarea" />
-  )
+    <div className="textarea">
+      <textarea
+        value={text}
+        onChange={handleOnChange}
+        placeholder="Enter Your Text"
+        spellCheck="false"
+      />
+      {warningText ? <Warning warningText={warningText} /> : null}
+    </div>
+  );
 }
