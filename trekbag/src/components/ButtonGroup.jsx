90c74febfe8f680d4cb2c46c1785c852e0c775aa
Spencer Attick <spencer.attick@gmail.com>
2025-04-20 13:51:30 -0400

    dynamically add buttons

--- trekbag/src/components/ButtonGroup.jsx
+++ trekbag/src/components/ButtonGroup.jsx
@@ -1,10 +1,22 @@
 import Button from "./Button";
 
+const secondaryButtons = [
+  "Mark All Complete",
+  "Mark All Incomplete",
+  "Reset to Initial",
+  "Remove All Items",
+];
+
 export default function ButtonGroup() {
-  return <section className="button-group">
-    <Button type='secondary'>Mark All Complete</Button>
-    <Button type='secondary'>Mark All Incomplete</Button>
-    <Button type='secondary'>Reset to Initial</Button>
-    <Button type='secondary'>Remove All Items</Button>
-  </section>;
+  return (
+    <section className="button-group">
+      {secondaryButtons.map((buttonText) => {
+        return (
+          <Button type="secondary" key={buttonText}>
+            {buttonText}
+          </Button>
+        );
+      })}
+    </section>
+  );
 }
