ccb675bd9902442e488510a3eb5bb90c8cf6d378
Spencer Attick <spencer.attick@gmail.com>
2025-04-23 06:44:01 -0400

    initial setup

---
+++ corp-comments/src/components/HashtagList.tsx
@@ -0,0 +1,12 @@
+export default function HashtagList() {
+  return (
+    <ul className="hashtags">
+        <li>
+          <button>#ByteGrad</button>
+        </li>
+        <li>
+          <button>#Nike</button>
+        </li>
+    </ul>
+  )
+}
