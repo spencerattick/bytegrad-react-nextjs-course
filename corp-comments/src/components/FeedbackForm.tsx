f743d7e79fd5774151b3d5741ede15ad69f2d9b5
Spencer Attick <spencer.attick@gmail.com>
2025-04-24 06:11:44 -0400

    manage controlled input

--- corp-comments/src/components/FeedbackForm.tsx
+++ corp-comments/src/components/FeedbackForm.tsx
@@ -1,16 +1,38 @@
+import { useState } from "react";
+import { MAX_CHARACTERS } from "../lib/constants";
+
+
 export default function FeedbackForm() {
+  const [text, setText] = useState("");
+
+  const remainingCharCount = MAX_CHARACTERS - text.length
+
+  const handleTextOnChange = (event: any) => {
+    const newText = event.target.value
+
+    if (newText.length > MAX_CHARACTERS) {
+        return 
+    }
+    setText(newText)
+  }
   return (
     <form className="form">
-        <textarea name="" id="feedback-textarea" placeholder="give feedback" spellCheck={false}></textarea>
-        <label htmlFor="feedback-textarea">
-            Enter your feedback here - remember to #hashtag the company
-        </label>
-        <div>
-            <p className="u-italic">150</p>
-            <button>
-                <span>Submit</span>
-            </button>
-        </div>
+      <textarea
+        onChange={handleTextOnChange}
+        value={text}
+        id="feedback-textarea"
+        placeholder="give feedback"
+        spellCheck={false}
+      ></textarea>
+      <label htmlFor="feedback-textarea">
+        Enter your feedback here - remember to #hashtag the company
+      </label>
+      <div>
+        <p className="u-italic">{remainingCharCount}</p>
+        <button>
+          <span>Submit</span>
+        </button>
+      </div>
     </form>
-  )
+  );
 }
