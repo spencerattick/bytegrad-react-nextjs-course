8c77aebd0027a7a3b5cea63330f88e360339c9da
Spencer Attick <spencer.attick@gmail.com>
2025-04-25 06:22:16 -0400

    add loading spinner

--- corp-comments/src/components/FeedbackList.tsx
+++ corp-comments/src/components/FeedbackList.tsx
@@ -1,19 +1,26 @@
 import { useEffect, useState } from "react";
 import FeedbackItem from "./FeedbackItem";
+import Spinner from "./Spinner";
 
 export default function FeedbackList() {
   const [feedbackItems, setFeedbackItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
+    setIsLoading(true)
     fetch('https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks')
     .then(res => res.json())
     .then(data => {
       setFeedbackItems(data.feedbacks)
+      setIsLoading(false)
     })
   }, []);
 
   return (
     <ol className="feedback-list">
+      {
+        isLoading && <Spinner />
+      }
       {feedbackItems.map((feedbackItem) => {
         return (
           <FeedbackItem feedbackItem={feedbackItem} key={feedbackItem.id}></FeedbackItem>
