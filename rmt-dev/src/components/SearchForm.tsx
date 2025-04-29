342dfc2169bfdbaa295f50456a19024ade2a2b62
Spencer Attick <spencer.attick@gmail.com>
2025-04-29 09:47:52 -0400

    fetch data from search

--- rmt-dev/src/components/SearchForm.tsx
+++ rmt-dev/src/components/SearchForm.tsx
@@ -1,6 +1,30 @@
+import { useEffect, useState } from "react";
+
 export default function SearchForm() {
+  const [searchText, setSearchText] = useState("");
+  const [jobItems, setJobItems] = useState([]);
+
+  console.log(jobItems)
+
+  useEffect(() => {
+    if (!searchText) return;
+
+    const fetchData = async () => {
+      const res = await fetch(
+        `https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${searchText}`
+      );
+      const data = await res.json();
+      setJobItems(data.jobItems);
+    };
+    fetchData();
+  }, [searchText]);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(e.target.value);
+    console.log(searchText);
+  };
   return (
-    <form action="#" className="search">
+    <form action="#" className="search" onSubmit={(e) => e.preventDefault()}>
       <button type="submit">
         <i className="fa-solid fa-magnifying-glass"></i>
       </button>
@@ -10,6 +34,8 @@ export default function SearchForm() {
         type="text"
         required
         placeholder="Find remote developer jobs..."
+        onChange={handleInputChange}
+        value={searchText}
       />
     </form>
   );
