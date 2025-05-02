1608de533edc04c3c1e9ae26877318928700815f
Spencer Attick <spencer.attick@gmail.com>
2025-05-02 17:00:11 -0400

    conditally render background color for selected job

--- rmt-dev/src/components/JobList.tsx
+++ rmt-dev/src/components/JobList.tsx
@@ -1,3 +1,4 @@
+import { useActiveId } from "../lib/hooks";
 import { JobItem } from "../lib/types";
 import JobListItem from "./JobListItem";
 import Spinner from "./Spinner";
@@ -8,11 +9,12 @@ type JobListProps = {
 };
 
 export function JobList({ jobItems, isLoading }: JobListProps) {
+  const activeId = useActiveId();
   return (
     <ul className="job-list">
       {isLoading && <Spinner />}
       {!isLoading &&
-        jobItems.map((jobItem) => <JobListItem jobItem={jobItem} key={jobItem.id}/>)}
+        jobItems.map((jobItem) => <JobListItem jobItem={jobItem} isActive={jobItem.id === activeId} key={jobItem.id}/>)}
     </ul>
   );
 }
