9c0660a706a45ffab60c236ec6430d4def65e763
Spencer Attick <spencer.attick@gmail.com>
2025-05-07 06:02:21 -0400

    stop favorite button event bubbling

--- rmt-dev/src/components/BookmarkIcon.tsx
+++ rmt-dev/src/components/BookmarkIcon.tsx
@@ -10,7 +10,14 @@ export default function BookmarkIcon({ id }: BookmarkIconProps) {
   const { bookmarkedIds, handleToggleBookmark } = useContext(BookmarksContext);
 
   return (
-    <button onClick={() => handleToggleBookmark(id)} className="bookmark-btn">
+    <button
+      onClick={(e) => {
+        handleToggleBookmark(id);
+        e.stopPropagation();
+        e.preventDefault();
+      }}
+      className="bookmark-btn"
+    >
       <BookmarkFilledIcon
         className={`${bookmarkedIds.includes(id) ? "filled" : ""}`}
       />
