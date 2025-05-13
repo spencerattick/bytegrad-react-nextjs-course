dc523731e7b318882905a473651ca9928af07400
Spencer Attick <spencer.attick@gmail.com>
2025-05-13 18:07:40 -0400

    add metadata and favicon

--- evento/src/app/page.tsx
+++ evento/src/app/page.tsx
@@ -6,9 +6,9 @@ export default function Home() {
       <h1 className="text-3xl lg:text-6xl font-bold tracking-tight">
         Find Events Around You
       </h1>
-      <p className="mb-12 mt-7 text-2xl lg:text-3xl opacity-75">Browse more than <span className="font-bold italic underline text-[#a4f839]">10,000 events</span> around you</p>
+      <p className="mb-12 mt-7 text-2xl lg:text-3xl opacity-75">Browse more than <span className="font-bold italic underline text-accent">10,000 events</span> around you</p>
       <form action="" className="w-full sm:w-[580px]">
-        <input placeholder="Search events in any city..." spellCheck={false} className="w-full h-16 rounded-lg bg-white/[7%] px-6 outline-none ring-[#a4f839]/50 transition focus:ring-2 focus:bg-white/10"/>
+        <input placeholder="Search events in any city..." spellCheck={false} className="w-full h-16 rounded-lg bg-white/[7%] px-6 outline-none ring-accent/50 transition focus:ring-2 focus:bg-white/10"/>
       </form>
       <section className="mt-4 flex gap-x-4 text-sm text-white/50">
         <p >Popular:</p>
