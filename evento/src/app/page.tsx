28b2823f469a1224ac0a4454c7ca96c900e305b7
Spencer Attick <spencer.attick@gmail.com>
2025-05-12 20:57:00 -0400

    style the Home component

--- evento/src/app/page.tsx
+++ evento/src/app/page.tsx
@@ -2,16 +2,17 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Find Events Around You</h1>
-      <p>Browse more than 10,000 events around you</p>
-      textinput
-      <form action="">
-        <input placeholder="Search events in any city..." spellCheck={false} />
+    <main className="flex flex-col items-center pt-36 px-3">
+      <h1 className="text-3xl lg:text-6xl font-bold tracking-tight">
+        Find Events Around You
+      </h1>
+      <p className="mb-12 mt-7 text-2xl lg:text-3xl opacity-75">Browse more than <span className="font-bold italic underline text-[#a4f839]">10,000 events</span> around you</p>
+      <form action="" className="w-full sm:w-[580px]">
+        <input placeholder="Search events in any city..." spellCheck={false} className="w-full h-16 rounded-lg bg-white/[7%] px-6 outline-none ring-[#a4f839]/50 transition focus:ring-2 focus:bg-white/10"/>
       </form>
-      <section>
-        <p>Popular:</p>
-        <div>
+      <section className="mt-4 flex gap-x-4 text-sm text-white/50">
+        <p >Popular:</p>
+        <div className="space-x-2 font-semibold">
           <Link href="/events/austin">Austin</Link>
           <Link href="/events/seattle">Seattle</Link>
         </div>
