ac919be703359a558ff3671c669a9844e74e8318
Spencer Attick <spencer.attick@gmail.com>
2025-05-14 06:44:40 -0400

    control form input

--- evento/src/app/page.tsx
+++ evento/src/app/page.tsx
@@ -1,3 +1,4 @@
+import SearchForm from "@/components/search-form";
 import Link from "next/link";
 
 export default function Home() {
@@ -6,12 +7,16 @@ export default function Home() {
       <h1 className="text-3xl lg:text-6xl font-bold tracking-tight">
         Find Events Around You
       </h1>
-      <p className="mb-12 mt-7 text-2xl lg:text-3xl opacity-75">Browse more than <span className="font-bold italic underline text-accent">10,000 events</span> around you</p>
-      <form action="" className="w-full sm:w-[580px]">
-        <input placeholder="Search events in any city..." spellCheck={false} className="w-full h-16 rounded-lg bg-white/[7%] px-6 outline-none ring-accent/50 transition focus:ring-2 focus:bg-white/10"/>
-      </form>
+      <p className="mb-12 mt-7 text-2xl lg:text-3xl opacity-75">
+        Browse more than
+        <span className="font-bold italic underline text-accent">
+          10,000 events
+        </span>
+        around you
+      </p>
+      <SearchForm />
       <section className="mt-4 flex gap-x-4 text-sm text-white/50">
-        <p >Popular:</p>
+        <p>Popular:</p>
         <div className="space-x-2 font-semibold">
           <Link href="/events/austin">Austin</Link>
           <Link href="/events/seattle">Seattle</Link>
