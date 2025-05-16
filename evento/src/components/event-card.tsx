a95f04cf61d33e7060ba04aeaaeb0a3676c2413f
Spencer Attick <spencer.attick@gmail.com>
2025-05-16 10:26:44 -0400

    style event cards

--- evento/src/components/event-card.tsx
+++ evento/src/components/event-card.tsx
@@ -1,11 +1,19 @@
 import { EventoEvent } from "@/lib/types";
+import Image from "next/image";
 
 type EventCardProps = {
-    event: EventoEvent;
-}
+  event: EventoEvent;
+};
 
-export default function EventCard({event}: EventCardProps) {
+export default function EventCard({ event }: EventCardProps) {
   return (
-    <section>{event.name}</section>
-  )
+    <section className="flex flex-col flex-1 basis-80 h-[380px] max-w-[500px] bg-white/[3%] rounded-xl overflow-hidden">
+      <Image width={500} height={280} alt={event.name} src={event.imageUrl} className="h-[60%] object-fit"/>
+      <div className="flex flex-col flex-1 justify-center items-center">
+        <h2 className="text-2xt font-semibold">{event.name}</h2>
+        <p className="italic text-white/75">by {event.organizerName}</p>
+        <p className="text-sm text-white/50 mt-4">{event.location}</p>
+      </div>
+    </section>
+  );
 }
