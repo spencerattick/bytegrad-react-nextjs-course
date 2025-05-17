4ab08e251a20e39d6e7bb67c7f5049a52e737c0c
Spencer Attick <spencer.attick@gmail.com>
2025-05-17 17:35:58 -0400

    begin styling event page

--- evento/src/app/event/[slug]/page.tsx
+++ evento/src/app/event/[slug]/page.tsx
@@ -1,5 +1,39 @@
-export default function EventPage() {
+import Image from "next/image";
+
+type EventPageProps = {
+  params: {
+    slug: string;
+  };
+};
+
+export default async function EventPage({ params }: EventPageProps) {
+  const { slug } = params;
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
+  );
+  const event = await response.json();
   return (
-    <main>EventPage</main>
-  )
+    <main>
+      <section className="relative h-[361px] overflow-hidden">
+        <Image
+          src={event.imageUrl}
+          className="object-cover z-0 blur-3xl"
+          alt="event background image"
+          fill
+          quality={50}
+          sizes="(max-width: 1280px) 100vw, 1280px"
+          priority
+        />
+        <div className="z-1 relative">
+          <Image
+            src={event.imageUrl}
+            alt={event.name}
+            width={300}
+            height={201}
+          />
+        </div>
+      </section>
+      <div></div>
+    </main>
+  );
 }
