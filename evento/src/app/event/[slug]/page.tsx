49e1587bc6348d97b31296d8f6ce65a5d556ede1
Spencer Attick <spencer.attick@gmail.com>
2025-05-19 20:21:53 -0400

    finish event page

--- evento/src/app/event/[slug]/page.tsx
+++ evento/src/app/event/[slug]/page.tsx
@@ -41,15 +41,44 @@ export default async function EventPage({ params }: EventPageProps) {
                 day: "numeric",
               })}
             </p>
-            <H1 className="mb-2 mt-1 whitespace-nowrap lg:text-5xl">{event.name}</H1>
+            <H1 className="mb-2 mt-1 whitespace-nowrap lg:text-5xl">
+              {event.name}
+            </H1>
             <p className="whitespace-nowrap text-xl text-white/75">
               Organized by <span className="italic">{event.organizerName}</span>
             </p>
-            <button className="bg-white/20 text-lg capitalize bg-blur mt-5 lg:mt-auto w-[95vw] sm:w-full rounded-md border-white/10 border-2 py-2 state-effects">Get Tickets</button>
+            <button className="bg-white/20 text-lg capitalize bg-blur mt-5 lg:mt-auto w-[95vw] sm:w-full rounded-md border-white/10 border-2 py-2 state-effects">
+              Get Tickets
+            </button>
           </div>
         </div>
       </section>
-      <div></div>
+      <div className="min-h-[75vh] text-center px-5 py-16">
+        <Section>
+          <SectionHeading>About this event</SectionHeading>
+          <SectionContent>{event.description}</SectionContent>
+        </Section>
+        <Section>
+          <SectionHeading>Location</SectionHeading>
+          <SectionContent>{event.location}</SectionContent>
+        </Section>
+      </div>
     </main>
+  );
+}
+
+function Section({ children }: { children: React.ReactNode }) {
+  return <section className="mb-12">{children}</section>;
+}
+
+function SectionHeading({ children }: { children: React.ReactNode }) {
+  return <h2 className="text-2xl mb-8">{children}</h2>;
+}
+
+function SectionContent({ children }: { children: React.ReactNode }) {
+  return (
+    <p className="max-w-4xl mx-auto text-lg leading-8 text-white/75">
+      {children}
+    </p>
   );
 }
