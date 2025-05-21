3e761194ae5fa0910acb3cf1800ec9427b7cbca6
Spencer Attick <spencer.attick@gmail.com>
2025-05-21 06:35:22 -0400

    add suspense for events page

--- evento/src/app/events/[city]/page.tsx
+++ evento/src/app/events/[city]/page.tsx
@@ -1,6 +1,7 @@
 import EventsList from "@/components/events-list";
 import H1 from "@/components/h1";
-import { EventoEvent } from "@/lib/types";
+import { Suspense } from "react";
+import Loading from "./loading";
 
 type EventsPageProps = {
   params: {
@@ -11,18 +12,15 @@ type EventsPageProps = {
 export default async function EventsPage({ params }: EventsPageProps) {
   const { city } = params;
 
-  const response = await fetch(
-    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
-  );
-  const events: EventoEvent[] = await response.json();
-
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
       <H1 className="mb-28">
         {city === "all" && "All Events"}
         {city !== "all" && `Events in ${city[0].toUpperCase() + city.slice(1)}`}
       </H1>
-      <EventsList events={events} />
+      <Suspense fallback={<Loading />}>
+        <EventsList city={city} />
+      </Suspense>
     </main>
   );
 }
