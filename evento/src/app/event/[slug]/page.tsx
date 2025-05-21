f5e46953ac57da4238187b69d03c52725f80c3a2
Spencer Attick <spencer.attick@gmail.com>
2025-05-21 07:48:42 -0400

    add metadata to event pages

--- evento/src/app/event/[slug]/page.tsx
+++ evento/src/app/event/[slug]/page.tsx
@@ -1,13 +1,26 @@
 import H1 from "@/components/h1";
+import { capitalize } from "@/lib/utils";
+import { Metadata } from "next";
 import Image from "next/image";
 
-type EventPageProps = {
+type Props = {
   params: {
     slug: string;
   };
 };
 
-export default async function EventPage({ params }: EventPageProps) {
+export async function generateMetadata({params}: Props): Promise<Metadata> {
+  const { slug } = params;
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
+  );
+  const event = await response.json();
+  return {
+    title: event.name,
+  } 
+}
+
+export default async function EventPage({ params }: Props) {
   const { slug } = params;
   const response = await fetch(
     `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
