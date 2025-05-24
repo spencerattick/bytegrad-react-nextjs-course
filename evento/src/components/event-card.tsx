048b65066321db672ad194ad8d1e67159887fd31
Spencer Attick <spencer.attick@gmail.com>
2025-05-24 07:00:56 -0400

    add scroll effect

--- evento/src/components/event-card.tsx
+++ evento/src/components/event-card.tsx
@@ -1,14 +1,32 @@
+"use client";
+
 import { EventoEvent } from "@/lib/types";
+import { motion, useScroll } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
+import { useRef } from "react";
 
 type EventCardProps = {
   event: EventoEvent;
 };
 
+const MotionLink = motion(Link);
+
 export default function EventCard({ event }: EventCardProps) {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["0 1", "1.5 1"]
+  });
   return (
-    <Link href={`/event/${event.slug}`} className="flex-1 basis-80 h-[380px] max-w-[500px]">
+    <MotionLink
+      ref={ref}
+      href={`/event/${event.slug}`}
+      className="flex-1 basis-80 h-[380px] max-w-[500px]"
+      style={{
+        scale: scrollYProgress
+      }}
+    >
       <section className="w-full h-full flex flex-col bg-white/[3%] rounded-xl overflow-hidden relative state-effects">
         <Image
           width={500}
@@ -33,6 +51,6 @@ export default function EventCard({ event }: EventCardProps) {
           </p>
         </section>
       </section>
-    </Link>
+    </MotionLink>
   );
 }
