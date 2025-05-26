dcfb9fdc209f3dd0b7c453ca426543cde28410e1
Spencer Attick <spencer.attick@gmail.com>
2025-05-26 06:32:16 -0400

    switch to prisma call for getEvent

--- evento/src/lib/utils.ts
+++ evento/src/lib/utils.ts
@@ -1,13 +1,15 @@
 import clsx, { ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { EventoEvent } from "@prisma/client";
+import { EventoEvent, PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export function cn(...inputs: ClassValue[]) {
-   return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 export function capitalize(string: string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 export async function getEvents(city: string) {
@@ -15,13 +17,12 @@ export async function getEvents(city: string) {
     `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
   );
   const events: EventoEvent[] = await response.json();
-  return events
+  return events;
 }
 
 export async function getEvent(slug: string) {
-    const response = await fetch(
-        `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
-      );
-      const event: EventoEvent = await response.json();
-      return event  
+  const event = await prisma.eventoEvent.findUnique({
+    where: { slug: slug },
+  });
+  return event;
 }
