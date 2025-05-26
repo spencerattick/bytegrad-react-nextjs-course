a64739d7ac0382dc178be546b44e32a1dfb721dd
Spencer Attick <spencer.attick@gmail.com>
2025-05-26 06:47:48 -0400

    add notFound

--- evento/src/lib/utils.ts
+++ evento/src/lib/utils.ts
@@ -1,6 +1,7 @@
 import clsx, { ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 import prisma from "./db";
+import { notFound } from "next/navigation";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -26,5 +27,8 @@ export async function getEvent(slug: string) {
   const event = await prisma.eventoEvent.findUnique({
     where: { slug: slug },
   });
+  if (!event) {
+    return notFound()
+  }
   return event;
 }
