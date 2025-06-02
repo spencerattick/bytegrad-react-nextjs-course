061871c14dccf230209a83fd6b11d57309459465
Spencer Attick <spencer.attick@gmail.com>
2025-06-02 05:47:53 -0400

    break dashboard into multiple components

--- petsoft/src/app/(app)/app/dashboard/page.tsx
+++ petsoft/src/app/(app)/app/dashboard/page.tsx
@@ -1,19 +1,12 @@
+import Branding from "@/components/branding";
+import Stats from "@/components/stats";
+
 export default function Page() {
   return (
     <main>
       <div className="flex items-center justify-between text-white py-8">
-        <section>
-          <h1 className="font-medium text-2xl leading-6">
-            Pet<span className="font-semibold">Soft</span>
-          </h1>
-          <p className="text-lg opacity-80">
-            Manage your pet daycare with ease
-          </p>
-        </section>
-        <section className="text-center">
-          <p className="text-2xl font-bold leading-6">2</p>
-          <p className="opacity-80">Current Guests</p>
-        </section>
+        <Branding />
+        <Stats />
       </div>
     </main>
   );
