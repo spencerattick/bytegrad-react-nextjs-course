b8e60f2ec55d7b6ec4d6b3d8df055f5747d958d0
Spencer Attick <spencer.attick@gmail.com>
2025-06-01 20:16:06 -0400

    add footer

--- petsoft/src/app/(app)/app/layout.tsx
+++ petsoft/src/app/(app)/app/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <>
       <BackgroundPattern />
 
-      <div className="max-w-[1050px] mx-auto px-4">
+      <div className="flex flex-col max-w-[1050px] mx-auto px-4 min-h-screen">
         <AppHeader />
         {children}
         <AppFooter />
--- petsoft/src/components/app-footer.tsx
+++ petsoft/src/components/app-footer.tsx
@@ -1,5 +1,9 @@
 export default function AppFooter() {
   return (
-    <div>AppFooter</div>
+    <footer className="mt-auto border-t border-black/5 py-5">
+        <small className="opacity-50">
+            &copy; 2025 ByteGrad. All rights reserved.
+        </small>
+    </footer>
   )
 }
