b36bb7787f9d7775753349a588ee87122af0a3bd
Spencer Attick <spencer.attick@gmail.com>
2025-06-01 20:24:04 -0400

    fill out dashboard page

--- petsoft/src/app/(app)/app/dashboard/page.tsx
+++ petsoft/src/app/(app)/app/dashboard/page.tsx
@@ -1,5 +1,20 @@
 export default function Page() {
   return (
-    <div> Dashboard Page</div>
-  )
+    <main>
+      <div className="flex items-center justify-between text-white py-8">
+        <section>
+          <h1 className="font-medium text-2xl leading-6">
+            Pet<span className="font-semibold">Soft</span>
+          </h1>
+          <p className="text-lg opacity-80">
+            Manage your pet daycare with ease
+          </p>
+        </section>
+        <section className="text-center">
+          <p className="text-2xl font-bold leading-6">2</p>
+          <p className="opacity-80">Current Guests</p>
+        </section>
+      </div>
+    </main>
+  );
 }
