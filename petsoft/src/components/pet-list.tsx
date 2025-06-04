219bfc312aaf98e6d4f4d31a222b2456c11e6574
Spencer Attick <spencer.attick@gmail.com>
2025-06-04 07:53:47 -0400

    type pets

--- petsoft/src/components/pet-list.tsx
+++ petsoft/src/components/pet-list.tsx
@@ -1,6 +1,11 @@
+import { Pet } from "@/lib/types";
 import Image from "next/image";
 
-export default function PetList({ pets }) {
+type PetListProps = {
+    pets: Pet[];
+}
+
+export default function PetList({ pets }: PetListProps) {
   return (
     <ul className="bg-white border-b border-black/[0.08]">
       {pets.map((pet) => (
