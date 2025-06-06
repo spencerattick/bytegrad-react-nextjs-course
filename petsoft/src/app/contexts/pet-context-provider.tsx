f17959b8e4adfd65b7d478f14294651db4cc7882
Spencer Attick <spencer.attick@gmail.com>
2025-06-06 12:17:59 -0400

    populate PetDetails

--- petsoft/src/app/contexts/pet-context-provider.tsx
+++ petsoft/src/app/contexts/pet-context-provider.tsx
@@ -4,10 +4,11 @@ import { Pet } from "@/lib/types";
 import React, { createContext, useState } from "react";
 
 type TPetContext = {
-    pets: Pet[];
-    selectedPetId: string | null;
-    handleChangeSelectedPetId: (id: string) => void;
-}
+  pets: Pet[];
+  selectedPetId: string | null;
+  selectedPet: Pet | undefined;
+  handleChangeSelectedPetId: (id: string) => void;
+};
 
 export const PetContext = createContext<TPetContext | null>(null);
 
@@ -20,19 +21,25 @@ export default function PetContextProvider({
   data,
   children,
 }: PetContextProviderProps) {
+  // state
   const [pets, setPets] = useState(data);
   const [selectedPetId, setSelectedPetId] = useState<string | null>(null);
 
+  // derived state
+  const selectedPet = pets.find((pet) => pet.id === selectedPetId);
+
+  // event handlers
   const handleChangeSelectedPetId = (id: string) => {
     setSelectedPetId(id);
-  }
+  };
 
   return (
     <PetContext.Provider
       value={{
         pets,
         selectedPetId,
-        handleChangeSelectedPetId
+        selectedPet,
+        handleChangeSelectedPetId,
       }}
     >
       {children}
--- petsoft/src/components/pet-details.tsx
+++ petsoft/src/components/pet-details.tsx
@@ -1,5 +1,41 @@
+"use client";
+
+import { usePetContext } from "@/lib/hooks";
+import Image from "next/image";
+
 export default function PetDetails() {
+  const { selectedPet } = usePetContext();
   return (
-    <section className="w-full h-full">Pet Details</section>
-  )
+    <section className="flex flex-col w-full h-full">
+      <div className="flex items-center bg-white px-8 py-5 border-b border-black/[0.08]">
+        <Image
+          src={selectedPet?.imageUrl}
+          alt="selected pet image"
+          height={75}
+          width={75}
+          className="h-[75px] w-[75px] rounded-full object-cover"
+        />
+        <h2 className="text-3xl font-semibold leading-7 ml-5">
+          {selectedPet?.name}
+        </h2>
+      </div>
+      <div className="flex justify-around py-10 px-5 text-center">
+        <div>
+          <h3 className="text-[13px] font-medium uppercase text-zinc-700">
+            Owner Name
+          </h3>
+          <p className="mt-1 text-lg text-zinc-800">{selectedPet?.ownerName}</p>
+        </div>
+        <div>
+          <h3 className="text-[13px] font-medium uppercase text-zinc-700">
+            Age
+          </h3>
+          <p className="mt-1 text-lg text-zinc-800">{selectedPet?.age}</p>
+        </div>
+      </div>
+      <section className="flex-1 bg-white px-7 py-5 rounded-md mb-9 mx-8 border border-black/[0.08]">
+        {selectedPet?.notes}
+      </section>
+    </section>
+  );
 }
