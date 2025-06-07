85490acebc8cb59bc830c4cb11da9e2d921596a8
Spencer Attick <spencer.attick@gmail.com>
2025-06-07 08:18:51 -0400

    break PetDetails into smaller components

--- petsoft/src/components/pet-details.tsx
+++ petsoft/src/components/pet-details.tsx
@@ -1,41 +1,60 @@
 "use client";
 
 import { usePetContext } from "@/lib/hooks";
+import { Pet } from "@/lib/types";
 import Image from "next/image";
 
 export default function PetDetails() {
   const { selectedPet } = usePetContext();
   return (
     <section className="flex flex-col w-full h-full">
-      <div className="flex items-center bg-white px-8 py-5 border-b border-black/[0.08]">
-        <Image
-          src={selectedPet?.imageUrl}
-          alt="selected pet image"
-          height={75}
-          width={75}
-          className="h-[75px] w-[75px] rounded-full object-cover"
-        />
-        <h2 className="text-3xl font-semibold leading-7 ml-5">
-          {selectedPet?.name}
-        </h2>
+      <TopBar pet={selectedPet} />
+      <OtherInfo pet={selectedPet} />
+      <Notes pet={selectedPet} />
+    </section>
+  );
+}
+
+type Props = {
+  pet: Pet;
+};
+
+function TopBar({ pet }: Props) {
+  return (
+    <div className="flex items-center bg-white px-8 py-5 border-b border-black/[0.08]">
+      <Image
+        src={pet?.imageUrl}
+        alt="selected pet image"
+        height={75}
+        width={75}
+        className="h-[75px] w-[75px] rounded-full object-cover"
+      />
+      <h2 className="text-3xl font-semibold leading-7 ml-5">{pet?.name}</h2>
+    </div>
+  );
+}
+
+function OtherInfo({ pet }: Props) {
+  return (
+    <div className="flex justify-around py-10 px-5 text-center">
+      <div>
+        <h3 className="text-[13px] font-medium uppercase text-zinc-700">
+          Owner Name
+        </h3>
+        <p className="mt-1 text-lg text-zinc-800">{pet?.ownerName}</p>
       </div>
-      <div className="flex justify-around py-10 px-5 text-center">
-        <div>
-          <h3 className="text-[13px] font-medium uppercase text-zinc-700">
-            Owner Name
-          </h3>
-          <p className="mt-1 text-lg text-zinc-800">{selectedPet?.ownerName}</p>
-        </div>
-        <div>
-          <h3 className="text-[13px] font-medium uppercase text-zinc-700">
-            Age
-          </h3>
-          <p className="mt-1 text-lg text-zinc-800">{selectedPet?.age}</p>
-        </div>
+      <div>
+        <h3 className="text-[13px] font-medium uppercase text-zinc-700">Age</h3>
+        <p className="mt-1 text-lg text-zinc-800">{pet?.age}</p>
       </div>
-      <section className="flex-1 bg-white px-7 py-5 rounded-md mb-9 mx-8 border border-black/[0.08]">
-        {selectedPet?.notes}
-      </section>
+    </div>
+  );
+}
+
+function Notes({ pet }: Props) {
+  return (
+    <section className="flex-1 bg-white px-7 py-5 rounded-md mb-9 mx-8 border border-black/[0.08]">
+      {pet?.notes}
     </section>
   );
 }
