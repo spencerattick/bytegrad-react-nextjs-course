f145399e577a9fe501dfb206812fe3270273aa25
Spencer Attick <spencer.attick@gmail.com>
2025-06-09 08:29:52 -0400

    update conditional logic for button rendering

--- petsoft/src/components/pet-button.tsx
+++ petsoft/src/components/pet-button.tsx
@@ -22,27 +22,6 @@ export default function PetButton({
   children,
   onClick,
 }: PetButtonProps) {
-  if (actionType === "add") {
-    return (
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button size="icon">
-            <PlusIcon className="w-6 h-6" />
-          </Button>
-        </DialogTrigger>
-
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Add a New Pet</DialogTitle>
-          </DialogHeader>
-          <PetForm></PetForm>
-        </DialogContent>
-      </Dialog>
-    );
-  }
-  if (actionType === "edit") {
-    return <Button variant="secondary">{children}</Button>;
-  }
   if (actionType === "checkout") {
     return (
       <Button variant="secondary" onClick={onClick}>
@@ -50,4 +29,26 @@ export default function PetButton({
       </Button>
     );
   }
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        {actionType === "add" ? (
+          <Button size="icon">
+            <PlusIcon className="w-6 h-6" />
+          </Button>
+        ) : (
+          <Button variant="secondary">{children}</Button>
+        )}
+      </DialogTrigger>
+
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>
+            {actionType === "add" ? "Add Pet" : "Edit Pet"}
+          </DialogTitle>
+        </DialogHeader>
+        <PetForm></PetForm>
+      </DialogContent>
+    </Dialog>
+  );
 }
