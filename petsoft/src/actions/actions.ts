d49f53a89b04df20d0dd2927493ab92f3d9cac30
Spencer Attick <spencer.attick@gmail.com>
2025-06-30 17:50:16 -0400

    update types to unknown on server

--- petsoft/src/actions/actions.ts
+++ petsoft/src/actions/actions.ts
@@ -7,7 +7,7 @@ import { petFormSchema } from "@/lib/validations";
 import { Pet } from "@prisma/client";
 import { revalidatePath } from "next/cache";
 
-export async function addPet(pet: PetEssentials) {
+export async function addPet(pet: unknown) {
   await sleep(1000);
 
   const validatedPet = petFormSchema.safeParse(pet)
@@ -31,7 +31,7 @@ export async function addPet(pet: PetEssentials) {
   revalidatePath("/app", "layout");
 }
 
-export async function editPet(petId: Pet['id'], newPetData: PetEssentials) {
+export async function editPet(petId: unknown, newPetData: unknown) {
   await sleep(1000);
   try {
     await prisma.pet.update({
@@ -47,7 +47,7 @@ export async function editPet(petId: Pet['id'], newPetData: PetEssentials) {
   revalidatePath("/app", "layout");
 }
 
-export async function deletePet(petId: Pet['id']) {
+export async function deletePet(petId: unknown) {
   await sleep(1000);
   try {
     await prisma.pet.delete({
