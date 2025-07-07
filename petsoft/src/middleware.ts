1c273773732d601359e26d77d3fefecfb66324e9
Spencer Attick <spencer.attick@gmail.com>
2025-07-07 16:28:43 -0700

    initialize middleware

---
+++ petsoft/src/middleware.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+
+export function middleware(request: Request) {
+
+    return NextResponse.next()
+}
+
+export const config = {
+    matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  };
