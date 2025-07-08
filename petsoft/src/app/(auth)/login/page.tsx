286fd795d676446ba6597af4f520038f72603452
Spencer Attick <spencer.attick@gmail.com>
2025-07-08 06:40:39 -0700

    dynamic login signup button

--- petsoft/src/app/(auth)/login/page.tsx
+++ petsoft/src/app/(auth)/login/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
   return (
     <main>
       <H1 className="mb-5 text-center">Log In</H1>
-      <AuthForm />
+      <AuthForm type="logIn"/>
       <p className="mt-6 text-sm text-zinc-500">
         No account yet? <Link href="/signup" className="font-medium">Sign up</Link>
       </p>
--- petsoft/src/components/auth-form.tsx
+++ petsoft/src/components/auth-form.tsx
@@ -2,18 +2,22 @@ import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 
-export default function AuthForm() {
+type AuthFormProps = {
+  type: "logIn" | "signUp";
+};
+
+export default function AuthForm({ type }: AuthFormProps) {
   return (
     <form className="space-y-2">
-        <div className="space-y-1">
-            <Label htmlFor="email">Email</Label>
-            <Input id="email" type="email" />
-        </div>
-        <div className=" mb-4 mt-2 space-y-1">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" />
-        </div>
-        <Button>Log In</Button>
+      <div className="space-y-1">
+        <Label htmlFor="email">Email</Label>
+        <Input id="email" type="email" />
+      </div>
+      <div className=" mb-4 mt-2 space-y-1">
+        <Label htmlFor="password">Password</Label>
+        <Input id="password" type="password" />
+      </div>
+      <Button>{type === "logIn" ? "Log In" : "Sign Up"}</Button>
     </form>
-  )
+  );
 }
