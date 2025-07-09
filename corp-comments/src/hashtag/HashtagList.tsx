7a755b1c859e02ddc83b6325d8663173619b9a2f
Spencer Attick <spencer.attick@gmail.com>
2025-07-09 11:23:38 -0700

    add auth secret

--- corp-comments/src/hashtag/HashtagList.tsx
+++ corp-comments/src/hashtag/HashtagList.tsx
@@ -1,9 +1,7 @@
-// import { useFeedbackItemsContext } from "../lib/hooks";
 import { useFeedbackItemsStore } from "../stores/feedbackItemsStore";
 import HashTagItem from "./HashTagItem";
 
 export default function HashtagList() {
-  // const { companyList, handleCompanyClick } = useFeedbackItemsContext();
   const getCompanyList = useFeedbackItemsStore(store => store.getCompanyList());
   const selectCompany = useFeedbackItemsStore(store => store.selectCompany);
   return (
