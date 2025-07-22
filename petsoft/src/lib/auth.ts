b396f7d6997bc90db465d7f02ac725ed42752922
Spencer Attick <spencer.attick@gmail.com>
2025-07-22 07:17:28 -0700

    clean up unused imports in auth

--- petsoft/src/lib/auth.ts
+++ petsoft/src/lib/auth.ts
@@ -1,8 +1,6 @@
 import NextAuth, { NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
-import prisma from "./db";
 import bcrypt from "bcryptjs";
-import { get } from "http";
 import { getUserByEmail } from "./server-utils";
 import { authSchema } from "./validations";
 
