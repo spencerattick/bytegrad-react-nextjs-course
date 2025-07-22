68eec22309a7b0f4c023f71e1986764f324c1f49
Spencer Attick <spencer.attick@gmail.com>
2025-07-22 07:15:29 -0700

    validate sign up form

--- petsoft/src/actions/actions.ts
+++ petsoft/src/actions/actions.ts
@@ -26,15 +26,27 @@ export async function logOut() {
   await signOut({ redirectTo: "/" });
 }
 
-export async function signUp(formData: FormData) {
-  const hashedPassword = await bcrypt.hash(
-    formData.get("password") as string,
-    10
-  );
+export async function signUp(formData: unknown) {
+  if (!(formData instanceof FormData)) {
+    return {
+      message: "Invalid form data",
+    };
+  }
+  const formDataEntries = Object.fromEntries(formData.entries());
+  const validatedFormData = await authSchema.safeParse(formDataEntries);
+  if (!validatedFormData.success) {
+    return {
+      message: "Invalid form data",
+    };
+  }
+
+  const { email, password } = validatedFormData.data;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
 
   await prisma.user.create({
     data: {
-      email: formData.get("email") as string,
+      email,
       hashedPassword,
     },
   });
