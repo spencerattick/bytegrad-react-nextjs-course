a76ebe65f7539cf9189652db0078014344cac4ec
Spencer Attick <spencer.attick@gmail.com>
2025-07-29 14:24:33 -0700

    send back a 200 for the Stripe webhook

--- petsoft/src/app/api/stripe/route.ts
+++ petsoft/src/app/api/stripe/route.ts
@@ -2,4 +2,6 @@ export async function POST(request: Request) {
     const body = await request.text();
     console.log("Received body:", body);
 
+    return Response.json(null, {status: 200});
+
 }
