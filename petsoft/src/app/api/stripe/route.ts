3d7d965febda84abded178abb01490fe89e4af24
Spencer Attick <spencer.attick@gmail.com>
2025-07-29 14:42:32 -0700

    update db when user pays

--- petsoft/src/app/api/stripe/route.ts
+++ petsoft/src/app/api/stripe/route.ts
@@ -1,7 +1,16 @@
+import prisma from '@/lib/db'
+
 export async function POST(request: Request) {
-    const body = await request.text();
-    console.log("Received body:", body);
+  const data = await request.json();
 
-    return Response.json(null, {status: 200});
+  await prisma.user.update({
+    where: {
+        email: data.data.object.customer_email
+    },
+    data: {
+      hasAccess: true,
+    },
+  });
 
+  return Response.json(null, { status: 200 });
 }
